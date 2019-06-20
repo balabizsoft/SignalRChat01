97794ad70593cb9a797070bf47fccdb3397c09a3
bala <bala.bizsoft@gmail.com>
2019-06-20 11:38:18 +0800

    Add project files.

---
+++ WebApplication19/Hubs/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication19.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        public void fnTest()
+        {
+
+        }
+
+        public string getCompanyName()
+        {
+            return "abcStore";
+        }
+
+        public int addTwoNumber(int num1,int num2)
+        {
+            Clients.All.SendAsync("display", num1 + num2);
+            return num1 + num2;
+        }
+
+        public void sendMessage(string name,string msg)
+        {
+            Clients.All.SendAsync("display", name, msg);
+        }
+    }
+}
---
+++ WebApplication19/WWWRoot/js/mychat.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+
+    $('#btnSend').prop('disabled', true);
+
+    var cn = new signalR.HubConnectionBuilder().withUrl("/mychat").build();
+    cn.on('display', (name, msg) => {
+        var str = `${name} says ${msg}`;
+        $('#lstMessage').append(`<li>${str}</li>`);
+    });
+
+    cn.start().then(x => {
+        $('#btnSend').prop('disabled', false);
+        console.log("Connected");
+    });
+
+
+    $('#btnSend').click(x => {
+        var name = $('#txtName').val();
+        var msg = $('#txtMessage').val();
+        cn.invoke('sendMessage', name, msg);
+    });
+});
